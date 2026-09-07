1aef95c23f115bdc7832f858c7329839cf1f1a41
commonsphere1-sketch <commonsphere1@gmail.com>
2026-09-06 19:49:56 -0700

    Revert "Add a country compass page, reached from the countries header"
    This reverts commit 87235e01deaaa9014f24f298c9379366f6317251.

--- src/App.tsx
+++ src/App.tsx
@@ -49,11 +49,6 @@ const PlanetaryBoundariesPage = lazy(() =>
     default: m.PlanetaryBoundariesPage,
   })),
 );
-const PoliticalCompassPage = lazy(() =>
-  import("./pages/PoliticalCompassPage").then((m) => ({
-    default: m.PoliticalCompassPage,
-  })),
-);
 const CrimeStatsPage = lazy(() =>
   import("./pages/CrimeStatsPage").then((m) => ({ default: m.CrimeStatsPage })),
 );
@@ -100,10 +95,6 @@ export default function App() {
               <Route path="settings" element={<SettingsPage />} />
               <Route path="states" element={<StatesPage />} />
               <Route path="countries" element={<CountriesPage />} />
-              <Route
-                path="political-compass"
-                element={<PoliticalCompassPage />}
-              />
               <Route path="cities" element={<CitiesPage />} />
               <Route path="economies" element={<EconomiesPage />} />
               <Route path="policy" element={<PolicyPage />} />
